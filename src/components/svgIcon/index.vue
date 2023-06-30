9a01a3bb0a669e62d018eb08ef7675d54ecf901a
mr_yichen <2151318772@qq.com>
2023-06-30 11:25:07 +0800

    改变tagview样式

---
+++ src/components/svgIcon/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <i :class="getIconfont" :style="getSTyle"></i>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps({
+    name: {
+        type: String
+    },
+    size: {
+        type: Number,
+        default: () => 14
+    },
+    color: {
+        type: String,
+        default: () => 'var(--wang-menu-color)'
+    }
+})
+
+const getIconfont = computed(() => {
+    return props?.name
+})
+
+const getSTyle = computed(() => {
+    return { color: props?.color, FontSize: props?.size }
+})
+</script>
+
+<style scoped lang="scss"></style>
