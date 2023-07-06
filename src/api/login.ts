fd24598304c0be62456427083cf2971dc7e6f510
mr_yichen <2151318772@qq.com>
2023-07-06 10:33:07 +0800

    登录接口的获取

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,4 +1,5 @@
 import { Jsons } from "../typings/global"
+import http from '/@/units/requerst'
 
 /**
  * 登录操作
@@ -7,6 +8,20 @@ import { Jsons } from "../typings/global"
  */
 export const getLogin = (username: string, password: string): Promise<any> => {
     return new Promise<Jsons<userInfo>>((resolve, reject) => {
-
+        http({
+            url: 'http://#',
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: {
+                username,
+                password
+            }
+        }).then(res => {
+            resolve(res)
+        }).catch(err => {
+            reject(err)
+        })
     })
 }
